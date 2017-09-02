e222de18a0a5ce43769e98e6ba563ece386561af
Dhairya Patel <31182647+dpacpop@users.noreply.github.com>
2017-09-03 00:43:41 +0530

    Create Question5.cpp

---
+++ Question5.cpp
@@ -0,0 +1,18 @@
+# include<iostream>
+# include<conio.h>
+using namespace std;
+int main()
+{
+	int a=0;
+	cout<<"enter the number\n";
+	cin>>a;
+	if((a%2)==0)
+	{
+		cout<<"the number is even.";
+	}
+	else if((a%2)!=0)
+	{
+		cout<<"the number odd.";
+	}
+	return 0;
+}
