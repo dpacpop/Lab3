11437ee28ecc759efb41f90621f88237eeef4932
Dhairya Patel <31182647+dpacpop@users.noreply.github.com>
2017-09-04 13:28:12 +0530

    Create Question10.cpp

---
+++ Question10.cpp
@@ -0,0 +1,22 @@
+# include<iostream>
+# include<conio.h>
+using namespace std;
+int main()
+{
+	char a;
+	cout<<"enter the character\n";
+	cin>>a;
+	if(a>=65 && a<=90)
+	{
+		cout<<"the character is in upper case.";
+	}
+	else if(a>=97 && a<=122)
+	{
+		cout<<"the caharacter is in lower case.";
+	}
+	else
+	{
+		cout<<"the character is not an alphabet";
+	}
+	return 0;
+}
