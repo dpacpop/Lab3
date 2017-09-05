96c12c8e2c8939d16e0172f30569a2fc0ac76dd1
Dhairya Patel <31182647+dpacpop@users.noreply.github.com>
2017-09-05 20:11:11 +0530

    Create Question15.cpp

---
+++ Question15.cpp
@@ -0,0 +1,17 @@
+# include<iostream>
+# include<conio.h>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"enter the sides of the triangle\n";
+	cin>>a,b,c;
+	if(a>0 && b>0 && c>0)
+	{
+		if((a+b)>c && (b+c)>a && (a+c)>b)
+		cout<<"the triangle is valid.";
+	}
+	else
+	cout<<"the triangle is not valid.";
+	return 0;
+}
