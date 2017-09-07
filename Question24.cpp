d4cdeb55d86c1e5d5c3d6888b85ec105b305b5f3
Dhairya Patel <31182647+dpacpop@users.noreply.github.com>
2017-09-07 22:03:27 +0530

    Create Question24.cpp

---
+++ Question24.cpp
@@ -0,0 +1,13 @@
+# include<iostream>
+# include<conio.h>
+using namespace std;
+int main()
+{
+	char c='a';
+	cout<<"the alphabets are\n";
+	while(c<='z')
+	{
+		cout<<c<<"\n";
+		c++;
+	}
+	return(0);
