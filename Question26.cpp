b1f71824ddf04de41d9ff963304869ebe598f27f
Dhairya Patel <31182647+dpacpop@users.noreply.github.com>
2017-09-07 22:06:29 +0530

    Create Question26.cpp

---
+++ Question26.cpp
@@ -0,0 +1,13 @@
+# include<iostream>
+# include<conio.h>
+using namespace std;
+int main()
+{
+	int a=1;
+	cout<<"the odd numbers between 1 and 100 are\n";
+	while(a<=100)
+	{
+		cout<<a<<"\n";
+		a=a+2;
+	}
+	return(0);
