b839024b573b9fbfd40bc9f1d740912ca6dcb3f1
Dhairya Patel <31182647+dpacpop@users.noreply.github.com>
2017-09-07 20:21:32 +0530

    Create Question22.cpp

---
+++ Question22.cpp
@@ -0,0 +1,15 @@
+# include<iostream>
+# include<conio.h>
+using namespace std;
+int main()
+{
+	int i=1,n=0;
+	cout<<"enter the natural number limit\n";
+	cin>>n;
+	while(i<=n)
+	{
+		cout<<i<<"\n";
+		i++;
+	}
+	return(0);
+}
