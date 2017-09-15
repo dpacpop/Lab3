9764989c1c732ac1fc140ae1a6463e6affa597cd
Dhairya Patel <31182647+dpacpop@users.noreply.github.com>
2017-09-15 21:27:51 +0530

    Create Question64.cpp

---
+++ Question64.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+#include<math.h>
+using namespace std;
+void main()
+{
+	long int octnum, decnum=0;
+	int i=0;
+	cout<<"Enter any Octal Number : ";
+	cin>>octnum;
+	while(octnum!=0)
+	{
+		decnum = decnum + (octnum%10) * pow(8, i);
+		i++;
+		octnum=octnum/10;
+	}
+	cout<<"Equivalent Decimal Value = "<<decnum;
+}
